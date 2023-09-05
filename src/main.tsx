2544c571b8ff5ff54146cf6e45322f43bc51b88c
DESKTOP-C07FJRA\web <283308784@qq.com>
2023-09-05 10:06:54 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,18 @@ import "./index.css";
 import { extendTheme } from '@chakra-ui/react'
 
 const colors = {
-  primary: {
-    100: '#4D9645',
-  }
+  primary:'#B60A21',
+  primary100:'#5C0015',
+  card: '#2A2A2A',
+  text100: '#CCCCCC',
+  text200: '#808080',
+  text300: '#1A1A1A',
+  text400: '#0D0D0D',
+  success: '#30B85D',
+  warning: '#F6FD29',
+  danger: "#FF1656",
+  selected: '#456EFF',
+  bgColor: '#151515'
 }
 
 const theme = extendTheme({colors})
